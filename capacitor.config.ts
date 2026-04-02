bac5e234d55e385e55c801635a1b50b4ca71f141
larcosmeyba <mleyba.cpt@gmail.com>
2026-04-02 16:34:23 -0700

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,12 +1,12 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.bf8fd03f0e474e05b2a30c914e6bd586',
-  appName: 'helping-hive-hub',
+  appId: 'com.helpthehive',
+  appName: 'Help the Hive',
   webDir: 'dist',
   server: {
-    url: 'https://bf8fd03f-0e47-4e05-b2a3-0c914e6bd586.lovableproject.com?forceHideBadge=true',
-    cleartext: true
+    url: 'https://www.helpthehive.com',
+    cleartext: false
   }
 };
 
