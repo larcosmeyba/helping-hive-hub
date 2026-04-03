bd0f91042b429861242b009debb22a17661f18f0
larcosmeyba <mleyba.cpt@gmail.com>
2026-04-03 09:17:28 -0700

    Enhance capacitor.config.ts with server and plugin options
    Add server configuration and SplashScreen plugin settings.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,17 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.helpthehive',
   appName: 'Help the Hive',
-  webDir: 'dist'
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  },
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 0,
+      autoHide: true,
+      androidScaleType: 'CENTER_CROP'
+    }
+  }
 };
 
 export default config;
