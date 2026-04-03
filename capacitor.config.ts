873ee144772988f71ded26966af3bff35d8f1307
larcosmeyba <mleyba.cpt@gmail.com>
2026-04-03 09:02:49 -0700

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,11 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.helpthehive',
   appName: 'Help the Hive',
-  webDir: 'dist',
-  server: {
-    url: 'https://helpthehive.com',
-    cleartext: false
-  }
+  webDir: 'dist'
 };
 
 export default config;
